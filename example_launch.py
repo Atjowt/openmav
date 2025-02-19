b584ed14a1faa5df4b17e441020ade64d7e1e922
atjowt <elliot.skogsberg@gmail.com>
2025-02-19 16:01:25 +0100

    add more examples

---
+++ example_launch.py
@@ -0,0 +1,11 @@
+import openmav
+
+def main():
+    """
+    Example of launching FlightGear on the default port
+    """
+    print('Launching FlightGear...')
+    _ = openmav.launch()
+
+if __name__ == '__main__':
+    main()
