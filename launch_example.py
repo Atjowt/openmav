d3e473bc10fa36c3f36a29f34ee774e68e047f92
isak.livner.makitalo@gmail.com <isak.livner.makitalo@gmail.com>
2025-04-06 22:37:11 +0200

    startup parameters from the command line

--- launch_example.py
+++ launch_example.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 
 import openmav
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="The program launches FlightGear with the specifiec startup parameters."
+    )
+    parser.add_argument("--aircraft", type=str, default=openmav.aircraft.F16_BLOCK_30, help="Specifies what aircraft model to use.")
+    parser.add_argument("--altitude", type=float, default=7000.0, help="Specifies the initial altitude.")
+    parser.add_argument("--latitude", type=float, default=60.8, help="Specifies the initial latitude.")
+    parser.add_argument("--longitude", type=float, default=17.1, help="Specifies the initial longitude.")
+    parser.add_argument("--heading", type=float, default=180.0, help="Specifies the initial heading (in degrees).")
+    parser.add_argument("--speed", type=float, default=400.0, help="Specifies the initial speed.")
+    args = parser.parse_args()
 
     print('Launching FlightGear...')
 
     launch_options = openmav.LaunchOptions (
-        aircraft=openmav.aircraft.F16_BLOCK_30,
-        altitude=7000.0,
-        latitude=60.8,
-        longitude=17.1,
-        heading=180.0,
-        speed=400.0,
+        aircraft=args.aircraft,
+        altitude=args.altitude,
+        latitude=args.latitude,
+        longitude=args.longitude,
+        heading=args.heading,
+        speed=args.speed,
         input=openmav.SocketOptions(port=5400, rate=10),
         output=openmav.SocketOptions(port=5500, rate=10),
     )
